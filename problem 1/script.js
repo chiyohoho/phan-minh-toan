a66ac1a54f50ae40765d47183a9861082f865c0d
Chiyohoho <whiteAlbum2.2294@gmail.com>
2024-04-22 13:23:23 +0700

    fix code

--- problem 1/script.js
+++ problem 1/script.js
@@ -3,18 +3,18 @@ const sumA = (n) => {
     for (let i = 0; i <= n; i++) {
         sum += i
     }
-    console.log('check sum cách A:', sum)
+    console.log('check sum A:', sum)
 }
 sumA(5)
 
 const sumB = (n) => {
     const sum = Array.from({ length: n }, (_, i) => i).reduce((sum, i) => sum + i, n)
-    console.log('check sum cách B:', sum)
+    console.log('check sum B:', sum)
 }
 sumB(5)
 
 const sumC = (n) => {
     const sum = (n * (n + 1) / 2)
-    console.log('check sum cách C:', sum)
+    console.log('check sum C:', sum)
 }
 sumC(5)
