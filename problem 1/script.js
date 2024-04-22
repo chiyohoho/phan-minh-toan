2eca41d1c1eb207f6c4da169cbcfaf16584c33a8
Chiyohoho <whiteAlbum2.2294@gmail.com>
2024-04-22 13:33:19 +0700

    remove clg

--- problem 1/script.js
+++ problem 1/script.js
@@ -3,18 +3,15 @@ const sumA = (n) => {
     for (let i = 0; i <= n; i++) {
         sum += i
     }
-    console.log('check sum A:', sum)
 }
 sumA(5)
 
 const sumB = (n) => {
     const sum = Array.from({ length: n }, (_, i) => i).reduce((sum, i) => sum + i, n)
-    console.log('check sum B:', sum)
 }
 sumB(5)
 
 const sumC = (n) => {
     const sum = (n * (n + 1) / 2)
-    console.log('check sum C:', sum)
 }
 sumC(5)
